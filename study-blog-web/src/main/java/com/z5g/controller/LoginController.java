46ba87271e57bfe602a8d065fa165542b494fcd9
2429402946@qq.com <2429402946@qq.com>
2020-07-19 00:09:40 +0800

    用户测试

--- study-blog-web/src/main/java/com/z5g/controller/LoginController.java
+++ study-blog-web/src/main/java/com/z5g/controller/LoginController.java
@@ -18,6 +18,7 @@ import java.util.UUID;
 public class LoginController extends BaseController {
     @RequestMapping(value = "login", method = RequestMethod.POST)
     public JSONObject login(@RequestBody JSONObject jsonObject) {
+
         return new JSONObject();
     }
 }
